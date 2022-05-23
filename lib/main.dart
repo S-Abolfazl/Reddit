408b1f13f19f1b32abbbbcdbfb42ed1e513f229a
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-24 01:46:15 +0430

    colors and other...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -33,15 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
   //MaterialColor Color = Colors.red;
   //TextStyle textStyle = textStyle.merge(Alignment.center);
@@ -50,6 +42,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -58,23 +51,44 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
+    return MaterialApp(
       //appBar: AppBar(title: Text(widget.title,style:TextStyle(color: Colors.purple,fontSize: 25,fontWeight: FontWeight.bold)),),
+      title:"Reddit",
 
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
 
+      home:Scaffold(
       appBar: AppBar(
+        //leading: Icon(Icons.pending,color: Colors.indigo,),
+          leading: IconButton(
+            icon: Icon(
+              Icons.pending,
+              color: Colors.indigo,
+            ),
+            padding: EdgeInsets.only(right:30),
+            onPressed: () {
+              // do something
+            },
+          ),
+        backgroundColor: Color.fromRGBO(233, 188, 14, 0),
 
-        leading: Icon(Icons.pending),
-        backgroundColor: Colors.black38,
         title : Container(
+         // alignment: Alignment.center,
           width: 300,
           height: 40,
 
+            alignment: Alignment.center,
           decoration: BoxDecoration(
-          color: Colors.black12, borderRadius: BorderRadius.circular(10)),margin: EdgeInsets.only(left:200),
+          color: Colors.black12, borderRadius: BorderRadius.circular(100)),margin: EdgeInsets.only(right:50,top: 6),
+
 
           child: TextField(
+
             decoration: InputDecoration(
+
                 prefixIcon: Icon(Icons.search),
                 suffixIcon: IconButton(
                   icon: Icon(Icons.clear),
@@ -86,52 +100,89 @@ class _MyHomePageState extends State<MyHomePage> {
                 border: InputBorder.none
             ),
           )
+
                   ),
+        actions:<Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.indigo,
+            ),
+            padding: EdgeInsets.only(right:30),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
                 ),
 
 
-      body: Column(
-        children: [
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+
+        ),
+
+
+      decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFffadad),
+          Color(0xFFffd6a5),
+          Color(0xFFfdffb6),
+          Color(0xFFcaffbf),
+          Color(0xFF9bf6ff),
+          Color(0xFFa0c4ff),
+          Color(0xFFbdb2ff),
+          Color(0xFFffc6ff),
+          Color(0xFFfffffc),
         ],
+      )
+      ),
+
+
       ),
         bottomNavigationBar: BottomNavigationBar(
-          items: [
+          items:[
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.home,
                 color: Colors.lightGreen,
               ),
-              label: 'Home',
+              label: '',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.compass_calibration_sharp,
                 color: Colors.lightGreen,
               ),
-              label: 'Compass',
+              label: '',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.add,
                 color: Colors.lightGreen,
               ),
-              label: 'Add',
+              label: '',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.maps_ugc_sharp,
                 color: Colors.lightGreen,
               ),
-              label: 'Maps',
+              label: '',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.add_alert_sharp,
                 color: Colors.lightGreen,
               ),
-              label: 'Alert',
+              label: '',
             )
           ],
+        )
 
         ),
     );
